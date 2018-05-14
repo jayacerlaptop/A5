89ca7c51e4d78d4d66e0f433bf19b0f31e2ea66c
jayacerlaptop <39017749+jayacerlaptop@users.noreply.github.com>
2018-05-14 09:45:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const express = require('express');
- const app = express();
- 
- app.use(express.static(__dirname + '/dist/'));
- app.listen(process.env.PORT || 8080);
 
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/srvup'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/srvup/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
